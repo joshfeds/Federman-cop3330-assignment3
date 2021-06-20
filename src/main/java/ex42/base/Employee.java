b39f09bffff092e4ba8b21773279648cbb9e4370
joshfeds14 <joshfeds14@gmail,com>
2021-06-20 16:27:23 -0400

    Exercises 41-46 done

--- src/main/java/ex42/base/Employee.java
+++ src/main/java/ex42/base/Employee.java
@@ -9,7 +9,7 @@ public class Employee {
     public String first;
     public String last;
     public String salary;
-    Employee(String fname, String lname, String salary){
+    Employee(String lname, String fname, String salary){
         this.first = fname;
         this.last = lname;
         this.salary = salary;
--- src/test/java/ex42/base/StringBase42Test.java
+++ src/test/java/ex42/base/StringBase42Test.java
@@ -10,12 +10,12 @@ class StringBase42Test {
     void getTabularOutput_employee_count_less_than_file() {
         StringBase42 program = new StringBase42();
         Employee [] employees = new Employee[6];
-        employees[0] = new Employee("Joshua", "Federman", "-580345");
-        employees[1] = new Employee("Omni", "Man", "123456");
-        employees[2] = new Employee("Eli", "Manning", "200711");
-        employees[3] = new Employee("Johnny", "OnTheSpot", "0");
-        employees[4] = new Employee("Tony", "Soprano", "1000975");
-        employees[5] = new Employee("Jenna", "Bulldosa", "34268");
+        employees[0] = new Employee("Federman", "Joshua", "-580345");
+        employees[1] = new Employee("Man", "Omni", "123456");
+        employees[2] = new Employee("Manning", "Eli", "200711");
+        employees[3] = new Employee("OnTheSpot", "Johnny", "0");
+        employees[4] = new Employee("Soprano", "Tony", "1000975");
+        employees[5] = new Employee("Bulldosa", "Jenna", "34268");
         int i = 0;
         String output =  "Last      First     Salary\n" +
                          "--------------------------\n"+
@@ -34,17 +34,17 @@ class StringBase42Test {
     void getTabularOutput_employee_count_more_than_file() {
         StringBase42 program = new StringBase42();
         Employee [] employees = new Employee[11];
-        employees[0] = new Employee("Joshua", "Federman", "-580345");
-        employees[1] = new Employee("Omni", "Man", "123456");
-        employees[2] = new Employee("Eli", "Manning", "200711");
-        employees[3] = new Employee("Johnny", "OnTheSpot", "0");
-        employees[4] = new Employee("Tony", "Soprano", "1000975");
-        employees[5] = new Employee("Jenna", "Bulldosa", "34268");
-        employees[6] = new Employee("Milly", "Rock", "1456");
-        employees[7] = new Employee("Yick", "Yack", "246624");
-        employees[8] = new Employee("BLe", "Blu", "8643");
-        employees[9] = new Employee("Yo", "Mama", "1000000000000000000000000000000000");
-        employees[10] = new Employee("Big", "Fella", "224625254");
+        employees[0] = new Employee("Federman", "Joshua", "-580345");
+        employees[1] = new Employee("Man", "Omni", "123456");
+        employees[2] = new Employee("Manning", "Eli", "200711");
+        employees[3] = new Employee("OnTheSpot", "Johnny", "0");
+        employees[4] = new Employee("Soprano", "Tony", "1000975");
+        employees[5] = new Employee("Bulldosa", "Jenna", "34268");
+        employees[6] = new Employee("Rock", "Milly", "1456");
+        employees[7] = new Employee("Yack", "Yick", "246624");
+        employees[8] = new Employee("Blu", "BLe", "8643");
+        employees[9] = new Employee("Mama", "Yo", "1000000000000000000000000000000000");
+        employees[10] = new Employee("Fella", "Big", "224625254");
 
         String output =  "Last      First     Salary\n" +
                          "--------------------------\n" +
